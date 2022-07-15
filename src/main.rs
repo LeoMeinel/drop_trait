3448188f6cc83daaf6951100342d26bf1d9a5dad
Leopold Johannes Meinel <leo@meinel.dev>
2022-07-15 05:38:55 +0200

    Initial Commit
    Signed-off-by: Leopold Johannes Meinel <leo@meinel.dev>

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+/*
+ * drop_trait is a commandline application.
+ * Copyright © 2022 Leopold Meinel & contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://github.com/LeoMeinel/drop_trait/blob/main/LICENSE
+ */
+
+use std::fmt::{Display, Formatter};
+
+fn main() {
+    use_my_smart_pointer();
+}
+
+struct MySmartPointer {
+    data: String,
+}
+
+impl Drop for MySmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping MySmartPointer with data: {}", self.data);
+    }
+}
+
+impl Display for MySmartPointer {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.data)
+    }
+}
+
+fn use_my_smart_pointer() {
+    let read = MySmartPointer {
+        data: "read".to_string(),
+    };
+    let _not_read = MySmartPointer {
+        data: "not read".to_string(),
+    };
+    let manually_dropped = MySmartPointer {
+        data: "manually dropped".to_string(),
+    };
+    println!("read is: {}", read);
+    // explicit destructor calls are not allowed here -> could result in a double-free
+    drop(manually_dropped);
+    println!();
+}
